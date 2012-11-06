26b4d3216b9737384dae817bb4d352a46c577a74
David Nix <david@sheldonsowell.com>
2012-11-06 08:53:20 -0700

    Add core data stack to appdelegate.

--- XML to Core Data Sample App/XML to Core Data/DNAppDelegate.m
+++ XML to Core Data Sample App/XML to Core Data/DNAppDelegate.m
@@ -2,9 +2,17 @@
 //  DNAppDelegate.m
 //  XML to Core Data
 //
-//  Created by David on 11/5/12.
-//  Copyright (c) 2012 David Nix. All rights reserved.
-//
+/*
+ 
+ Copyright (C) 2012 David Nix
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
 
 #import "DNAppDelegate.h"
 
@@ -42,5 +50,105 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+#pragma mark -
+#pragma mark Core Data stack
+
+/**
+ Returns the managed object context for the application.
+ If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+ */
+- (NSManagedObjectContext *)managedObjectContext {
+#ifdef DEBUG
+    NSLog(@"ManagedObject Context Called.");
+#endif
+    
+    if (managedObjectContext_ != nil) {
+        return managedObjectContext_;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        managedObjectContext_ = [[NSManagedObjectContext alloc] init];
+        [managedObjectContext_ setPersistentStoreCoordinator:coordinator];
+    }
+    return managedObjectContext_;
+}
+
+
+/**
+ Returns the managed object model for the application.
+ If the model doesn't already exist, it is created from the application's model.
+ */
+- (NSManagedObjectModel *)managedObjectModel {
+#ifdef DEBUG
+    NSLog(@"ManagedObject Model Called.");
+#endif
+    
+    if (managedObjectModel_ != nil) {
+        return managedObjectModel_;
+    }
+    NSString *modelPath = [[NSBundle mainBundle] pathForResource:@"XML2CoreData" ofType:@"momd"];
+    NSURL *modelURL = [NSURL fileURLWithPath:modelPath];
+    managedObjectModel_ = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return managedObjectModel_;
+}
+
+
+/**
+ Added encryption for the persistent store.
+ The database is only decrypted after the Password Controller is dismissed.
+ If the Password Controller's password is compromised, an attacker will have full access to all the data, so it's important this password is
+ kept secret.
+ */
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    
+    if (persistentStoreCoordinator_ != nil) {
+        return persistentStoreCoordinator_;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"XML2CoreData.sqlite"];
+    
+    NSError *error = nil;
+    persistentStoreCoordinator_ = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![persistentStoreCoordinator_ addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        
+        //        // delete the store and start over
+        //        [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
+        //        [self persistentStoreCoordinator];
+        
+        
+        //        if (![persistentStoreCoordinator_ addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:nil]) {
+    }
+    
+    
+    
+    // set the file protection level for the persistent store, that way the file is hardware encrypted while the app is locked
+    // but this means the strength of the encryption is only as good as the user's iPhone passcode or password
+    // this ONLY works with iOS 4 or later.
+    // Even though we encrypt it above, this operation is cheap and adds 1 more layer of security.
+    // source:  https://nickharris.wordpress.com/2010/07/14/core-data-and-enterprise-iphone-applications-protecting-your-data/
+    NSError *dataProtectError = nil;
+    NSDictionary *fileAttributes = [NSDictionary dictionaryWithObject:NSFileProtectionComplete forKey:NSFileProtectionKey];
+    if (![[NSFileManager defaultManager] setAttributes:fileAttributes ofItemAtPath:[storeURL path] error:&dataProtectError]) {
+        // not going to warn the user since the store is encrypted as above
+        NSLog(@"Unresolved error %@, %@", dataProtectError, [dataProtectError userInfo]);
+    }
+    
+    
+    return persistentStoreCoordinator_;
+}
+
+
+#pragma mark -
+#pragma mark Application's Documents directory
+
+/**
+ Returns the URL to the application's Documents directory.
+ */
+- (NSURL *)applicationDocumentsDirectory {
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
 
 @end
