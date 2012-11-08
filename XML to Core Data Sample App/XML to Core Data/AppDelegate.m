2492d83058f98b28c5daf83415c9faf7daa326be
David Nix <david@sheldonsowell.com>
2012-11-08 16:14:50 -0700

    Fix #imports for new parse operation name.

--- XML to Core Data Sample App/XML to Core Data/AppDelegate.m
+++ XML to Core Data Sample App/XML to Core Data/AppDelegate.m
@@ -53,6 +53,10 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+#pragma mark -
+#pragma mark Test Data (for debugging of core data logic)
+
 -(void)addTestData {
     NSManagedObject *movie = [NSEntityDescription insertNewObjectForEntityForName:@"Movie" inManagedObjectContext:self.managedObjectContext];
     [movie setValue:@"Jerry Maguire" forKey:@"title"];
